c6cfd4442f55a1b82ede97af9fa69114bdebc168
andresramos2 <134411262+andresramos2@users.noreply.github.com>
2023-05-23 18:25:26 +0200

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    motor.MotorRun(motor.Motors.M1, motor.Dir.CW, 210)
+    pins.digitalWritePin(DigitalPin.P0, 1)
+})
+input.onButtonPressed(Button.B, function () {
+    motor.MotorRun(motor.Motors.M1, motor.Dir.CW, 0)
+    pins.digitalWritePin(DigitalPin.P0, 0)
+})
 basic.forever(function () {
     pins.digitalWritePin(DigitalPin.P0, 1)
-    basic.pause(200)
-    pins.digitalWritePin(DigitalPin.P0, 0)
     basic.pause(200)
 })
